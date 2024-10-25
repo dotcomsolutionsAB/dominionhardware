c55dc8c548bc9f5f6e0652fa7b24ee91e6ae581d
Shaqlin Mondal <79474217+ShaqlinMondal-creater@users.noreply.github.com>
2024-10-25 19:58:24 +0530

    Update add_products.php

--- api/add_products.php
+++ api/add_products.php
@@ -133,7 +133,7 @@ while ($row_1 = $query_1->fetch_assoc()) {
             $pos = $header_array['Product Name'];
             $name = addslashes($data[$pos]);
 
-            if ($sku != '' && !empty($sku) && !empty($name) && $sku != '') {
+            if ($sku != '' && !empty($sku)) {
 
                 $pos = $header_array['Merchant Item ID'];
                 $item_id = $data[$pos];
@@ -153,7 +153,7 @@ while ($row_1 = $query_1->fetch_assoc()) {
                 $pos = $header_array['Status'];
                 $product_status = $data[$pos];
 
-                if ($product_status == 'draft') {
+                if ($product_status == 'draft' && empty($name) && $name == '') {
                     $published = 0;
                 } else {
                     $published = 1;
