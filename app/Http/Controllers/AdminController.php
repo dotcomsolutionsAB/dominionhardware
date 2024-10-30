804c3d30abb030bee2f4da04228491856764eb82
Shaqlin Mondal <79474217+ShaqlinMondal-creater@users.noreply.github.com>
2024-10-30 16:22:25 +0530

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -184,7 +184,8 @@ class AdminController extends Controller
             ->orderBy('total', 'desc')
             ->limit(3)
             ->get();
-        $data['total_sale'] = Order::where('delivery_status', 'delivered')->sum('grand_total');
+        // $data['total_sale'] = Order::where('delivery_status', 'delivered')->sum('grand_total');
+        $data['total_sale'] = Order::where('delivery_status', '!=', 'cancelled')->sum('grand_total');
         $data['sale_this_month'] = Order::whereMonth('created_at', Carbon::now()->month)->sum('grand_total');
         $data['admin_sale_this_month'] = Order::select(DB::raw('COALESCE(users.user_type, "admin") as user_type'), DB::raw('COALESCE(SUM(grand_total), 0) as total_sale'))
             ->leftJoin('users', 'orders.seller_id', '=', 'users.id')
