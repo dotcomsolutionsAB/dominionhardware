c891349a65e42d64458c079fcd999f0156b7bfd3
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-11-02 15:43:30 +0530

    upd89

--- resources/views/frontend/checkout_debug.blade.php
+++ resources/views/frontend/checkout_debug.blade.php
@@ -8,21 +8,23 @@
     <h1>Checkout Debug Information</h1>
     
     <h2>Message:</h2>
-    <p>{{ $message }}</p>
+    <p>{{ $message ?? 'No message available' }}</p>
 
     <h2>Combined Order ID:</h2>
     <p>{{ $combined_order_id ?? 'N/A' }}</p>
 
     <h2>Session Data:</h2>
-    <pre>{{ print_r($session_data, true) }}</pre>
+    <pre>{!! print_r($session_data ?? 'No session data available', true) !!}</pre>
 
-    @if(isset($errors) && count($errors) > 0)
+    @if(isset($errors) && $errors->count() > 0)
         <h2>Errors:</h2>
         <ul>
             @foreach($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
         </ul>
+    @else
+        <p>No errors found.</p>
     @endif
 </body>
 </html>
